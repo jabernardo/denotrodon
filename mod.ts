c3e99eeedc49b07e22e1ce56d4b8bd9218efa2a6
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-05-30 12:33:36 +0800

    command instance

--- mod.ts
+++ mod.ts
@@ -1,6 +1,10 @@
-export type Command = (app: Denotrodon) => any;
+export type CommandAction = () => any;
 export interface CommandList {
   [key: string]: Command;
+}
+
+export interface Option {
+  [name: string]: string;
 }
 
 export interface Options {
@@ -21,26 +25,25 @@ export class Denotrodon {
   private arguments: string[] = [];
   private options: Options = {};
   private flags: Flags = {};
-  private silent: boolean = false;
 
   private _getOptions(): Options {
     const options: Options = {};
     const optionsPattern: RegExp = /^--(\S+)=(.*)/i;
     let tempArgs: string[] = [];
 
-    this.arguments.forEach(param => {
+    this.arguments.forEach((param) => {
       let results: RegExpExecArray | null = optionsPattern.exec(param);
 
       if (results) {
-        let [ str, key, val ] = results;
+        let [str, key, val] = results;
 
-        options[key] = val;
+        options[key.toLowerCase()] = val;
 
         return true;
       }
 
       tempArgs.push(param);
-    })
+    });
 
     this.arguments = tempArgs;
 
@@ -52,11 +55,11 @@ export class Denotrodon {
     const flagsPattern: RegExp = /^-(\S+)/i;
     let tempArgs: string[] = [];
 
-    this.arguments.forEach(param => {
+    this.arguments.forEach((param) => {
       let results: RegExpExecArray | null = flagsPattern.exec(param);
 
       if (results) {
-        let [ str, val ] = results;
+        let [str, val] = results;
 
         flags[val] = true;
 
@@ -64,31 +67,97 @@ export class Denotrodon {
       }
 
       tempArgs.push(param);
-    })
+    });
 
     this.arguments = tempArgs;
 
     return flags;
   }
 
-  constructor() {
-    this.arguments = Array.from(Deno.args);
-    this.commandActive = "default";
+  private async _runActive(): Promise<void> {
+    const command = this.commands[this.commandActive];
 
-    if (this.arguments.length && !this.commands[this.arguments[0]]) {
-      console.error("Command not found.");
-      // Deno.exit(1);
+    if (command) {
+      await command.exec(this);
     }
+  }
 
-    this.commandActive = this.arguments[0] || "default";
-    this.arguments.shift();
+  public has(flagName: string, defaultVal: boolean = false): boolean {
+    return this.flags[flagName] || defaultVal;
+  }
+
+  public option(optionName: string, defaultVal: any = null): any {
+    return this.options[optionName] || defaultVal;
+  }
+
+  run() {
+    this.arguments = Array.from(Deno.args);
+    this.commandActive = this.commandDefault;
+
+    if (this.arguments.length && this.commands[this.arguments[0]]) {
+      this.commandActive = this.arguments[0];
+      this.arguments.shift();
+    }
 
     this.options = this._getOptions();
     this.flags = this._getFlags();
 
     console.log(this.commandActive, this.arguments, this.options, this.flags);
+
+    this._runActive();
+  }
+
+  command(name: string, c: Command) {
+    if (this.commands[name.toLowerCase()]) {
+      throw new Error("Command already exists.");
+    }
+
+    this.commands[name] = c;
   }
 }
 
+export class Command {
+  optionsExpected: Option[] = [];
+  action: CommandAction;
+
+  constructor(action: CommandAction) {
+    this.action = action;
+  }
+
+  expects(option: Option): Command {
+    this.optionsExpected.push(option);
+    return this;
+  }
+
+  private checkExpectations(app: Denotrodon) {
+    this.optionsExpected.forEach((option) => {
+      Object.keys(option).forEach((key) => {
+        if (!app.option(key)) {
+          console.error(`Option "${key}" as "${option[key]}" expected.`);
+          Deno.exit(2);
+        }
+      });
+    });
+  }
+
+  async exec(app: Denotrodon) {
+    this.checkExpectations(app);
+
+    await this.action();
+  }
+}
 
 const app: Denotrodon = new Denotrodon();
+
+app.command(
+  "test",
+  new Command(function() {
+    console.log("hello", app.has("q"));
+  })
+    .expects({ "name": "Username", "age": "Age" }),
+);
+
+app.run();
+
+
+console.log(app);
